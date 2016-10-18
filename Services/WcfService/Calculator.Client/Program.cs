997ba2743cf3e8a74d2b116e0dda3d3f840e940e
Suchi Agicha <suchia@microsoft.com>
2016-10-18 13:07:46 -0700

    Corrected the License Information

--- Services/WcfService/Calculator.Client/Program.cs
+++ Services/WcfService/Calculator.Client/Program.cs
@@ -1,8 +1,7 @@
-﻿// ----------------------------------------------------------------------
-//  <copyright file="Program.cs" company="Microsoft">
-//       Copyright (c) Microsoft Corporation. All rights reserved.
-//  </copyright>
-// ----------------------------------------------------------------------
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 
 namespace Calculator.Client
 {
@@ -33,7 +32,6 @@ namespace Calculator.Client
     /// clients and attempts to reuse the clients for requests to the same service endpoint.
     /// Its using BasicHttpBinding.
     ///  </summary>
-
     public class CalculatorClient : ServicePartitionClient<WcfCommunicationClient<ICalculator>>, ICalculator
     {
         private static ICommunicationClientFactory<WcfCommunicationClient<ICalculator>> communicationClientFactory;
--- Services/WcfService/Calculator.Common/ICalculator.cs
+++ Services/WcfService/Calculator.Common/ICalculator.cs
@@ -1,8 +1,7 @@
-﻿// ----------------------------------------------------------------------
-//  <copyright file="ICalculator.cs" company="Microsoft">
-//       Copyright (c) Microsoft Corporation. All rights reserved.
-//  </copyright>
-// ----------------------------------------------------------------------
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 
 namespace Calculator.Common
 {
