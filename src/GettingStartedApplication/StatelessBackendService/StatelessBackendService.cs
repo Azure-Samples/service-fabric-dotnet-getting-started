8590fc8b876066dfd527c9cd28348f5b8639dcc2
Brahmnes Fung <bfung@microsoft.com>
2017-04-20 11:02:20 -0700

    Adding correlation support for stateless backend service

--- src/GettingStartedApplication/StatelessBackendService/StatelessBackendService.cs
+++ src/GettingStartedApplication/StatelessBackendService/StatelessBackendService.cs
@@ -7,6 +7,7 @@ namespace StatelessBackendService
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Fabric;
     using System.Threading;
     using System.Threading.Tasks;
@@ -14,6 +15,10 @@ namespace StatelessBackendService
     using Microsoft.ServiceFabric.Services.Communication.Runtime;
     using Microsoft.ServiceFabric.Services.Remoting.Runtime;
     using Microsoft.ServiceFabric.Services.Runtime;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.ServiceFabric;
 
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
@@ -21,15 +26,25 @@ namespace StatelessBackendService
     internal sealed class StatelessBackendService : StatelessService, IStatelessBackendService
     {
         private long iterations = 0;
+        private TelemetryClient telemetryClient;
 
         public StatelessBackendService(StatelessServiceContext context)
             : base(context)
         {
+            telemetryClient = new TelemetryClient(TelemetryConfiguration.Active);
+            FabricTelemetryInitializer.SetServiceCallContext(context);
         }
 
-        public Task<long> GetCountAsync()
+        public Task<long> GetCountAsync(string requestId, Dictionary<string, string> correlationContextHeader)
         {
-            return Task.FromResult(this.iterations);
+            var telemetry = new RequestTelemetry();
+
+            // Create a new activity for this new request, and end it when we finish processing
+            StartActivity("GetCountAsync", telemetry, requestId, correlationContextHeader);
+            return Task.FromResult(this.iterations).ContinueWith((task) => {
+                this.StopActivity(telemetry);
+                return task.Result;
+            });
         }
 
         /// <summary>
@@ -63,6 +78,60 @@ namespace StatelessBackendService
 
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }
+        }
+
+        private void StartActivity(string callerName, RequestTelemetry telemetry, string requestId, Dictionary<string, string> correlationContextHeader)
+        {
+            // Initialize all needed properties on the activity object
+            var activity = new Activity(callerName);
+            activity.SetParentId(requestId);
+            telemetry.Context.Operation.ParentId = requestId;
+            
+            foreach (KeyValuePair<string, string> pair in correlationContextHeader)
+            {
+                activity.AddBaggage(pair.Key, pair.Value);
+            }
+
+            activity.Start();
+
+            // Initialize all needed properties on the request telemetry object, and start tracking
+            telemetry.Id = activity.Id;
+            telemetry.Context.Operation.Id = activity.RootId;
+            telemetry.Context.Operation.ParentId = activity.ParentId;
+
+            this.telemetryClient.Initialize(telemetry);
+            telemetry.Start(Stopwatch.GetTimestamp());
+
+            //IHeaderDictionary responseHeaders = httpContext.Response?.Headers;
+            //if (responseHeaders != null &&
+            //    !string.IsNullOrEmpty(telemetry.Context.InstrumentationKey) &&
+            //    (!responseHeaders.ContainsKey(RequestResponseHeaders.RequestContextHeader) || HttpHeadersUtilities.ContainsRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey)))
+            //{
+            //    string correlationId = null;
+            //    if (this.correlationIdLookupHelper.TryGetXComponentCorrelationId(telemetry.Context.InstrumentationKey, out correlationId))
+            //    {
+            //        HttpHeadersUtilities.SetRequestContextKeyValue(responseHeaders, RequestResponseHeaders.RequestContextTargetKey, correlationId);
+            //    }
+            //}
+        }
+
+        private void StopActivity(RequestTelemetry telemetry)
+        {
+            // Stop the request telemetry tracking and log it with the telemetry client
+            telemetry.Stop(Stopwatch.GetTimestamp());
+            telemetry.Success = true;
+
+            if (string.IsNullOrEmpty(telemetry.Name))
+            {
+                telemetry.Name = "GET " + "GetCountAsync";
+            }
+
+            telemetry.HttpMethod = "GET";
+            telemetry.Url = new Uri("fabric:/GettingStartedApplication/StatelessBackendService/GetCountAsync");
+            telemetryClient.TrackRequest(telemetry);
+
+            // Stop and conclude the activity
+            Activity.Current.Stop();
         }
     }
 }
--- src/GettingStartedApplication/WebService/Controllers/StatelessBackendServiceController.cs
+++ src/GettingStartedApplication/WebService/Controllers/StatelessBackendServiceController.cs
@@ -11,8 +11,13 @@ namespace WebService.Controllers
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.ServiceFabric.Services.Remoting.Client;
     using StatelessBackendService.Interfaces;
+    using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Fabric;
     using System;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Extensibility;
 
     [Route("api/[controller]")]
     public class StatelessBackendServiceController : Controller
@@ -34,7 +39,52 @@ namespace WebService.Controllers
 
             IStatelessBackendService proxy = ServiceProxy.Create<IStatelessBackendService>(new Uri(serviceUri));
 
-            long result = await proxy.GetCountAsync();
+            // Create and start a new activity representing the beginning of this outgoing request
+            Activity activity = new Activity("HttpOut");
+            activity.Start();
+
+            DateTimeOffset startTime = DateTimeOffset.UtcNow;
+
+            // Extract the request id and correlation context headers so they can be passed to the callee, which
+            // will create the correlation
+            Activity currentActivity = Activity.Current;
+
+            string requestId = currentActivity.Id;
+            Dictionary<string, string> correlationContextHeader = new Dictionary<string, string>();
+            foreach (var pair in currentActivity.Baggage)
+            {
+                correlationContextHeader.Add(pair.Key, pair.Value);
+            }
+
+            long result = 0;
+            try
+            {
+                result = await proxy.GetCountAsync(requestId, correlationContextHeader).ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                //always stop activity if it was started
+                if (activity != null)
+                {
+                    activity.Stop();
+                }
+                DateTimeOffset endTime = DateTimeOffset.UtcNow;
+                DependencyTelemetry telemetry = new DependencyTelemetry(
+                    "HTTP", // dependencyTypeName
+                    serviceUri, // target
+                    "GET " + serviceUri, // dependencyName
+                    serviceUri, // data
+                    startTime, // startTime
+                    endTime - startTime, // duration
+                    "OK", // resultCode
+                    true); // success
+                TelemetryClient client = new TelemetryClient(TelemetryConfiguration.Active);
+                client.TrackDependency(telemetry);
+            }
 
             return this.Json(new CountViewModel() { Count = result });
         }
